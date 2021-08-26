16c250e757aceda21bc38b6c703bf14ad4f27689
ignored-comment <info.konham@gmail.com>
2021-08-26 18:09:22 -0400

    Create user validations; tests passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :email, uniqueness: true, presence: true
+    validates :password, presence: true, confirmation: true
+
+    has_secure_password
 end
