8e54e65562a5c5c07eb34d33fb8513202f653f7d
ignored-comment <info.konham@gmail.com>
2021-08-26 17:45:34 -0400

    Include name attribute for user factorybot model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
+    name { Faker::Movies::StarWars.character }
     email { Faker::Internet.email }
     password_digest { Faker::Internet.password }
     # api_key { Faker::Blockchain::Ethereum.address }
