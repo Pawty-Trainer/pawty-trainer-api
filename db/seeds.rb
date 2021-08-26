229927fd0a32fc4dfebb830092858a2554120470
ignored-comment <info.konham@gmail.com>
2021-08-26 18:02:41 -0400

    Create seed files for User and verify user attributes work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# 5.times do
+#   user = User.create(name: :user.name, email: :user.email, password_digest: :user.password_digest)
+# end
+
 5.times do
-  user = User.create(name: Faker::Name.name, email: Faker::Internet.email, password_digest: Faker::Internet.password)
+    User.create(
+        name: Faker::Movies::StarWars.character,
+        email: Faker::Internet.email,
+        password_digest: Faker::Internet.password
+    )
 end
