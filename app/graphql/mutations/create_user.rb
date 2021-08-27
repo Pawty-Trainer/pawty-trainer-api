f81c8becdea0517a2594030f73070a1c738cb667
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-27 15:37:12 -0600

    Implement create user mutations

---
+++ app/graphql/mutations/create_user.rb
@@ -0,0 +1,28 @@
+class Mutations::CreateUser < Mutations::BaseMutation
+  argument :name, String, required: true
+  argument :email, String, required: true
+  argument :password, String, required: true
+  argument :password_confirmation, String, required: true
+
+  field :user, Types::UserType, null: false
+  field :errors, [String], null: false
+
+  def resolve(name:, email:, password:, password_confirmation:)
+    user = User.new(name: name, email: email, password: password, password_confirmation: password_confirmation)
+    if user[:password] == user[:password_confirmation]
+        user.save
+    end
+
+    if user.save
+      {
+        user: user,
+        errors: []
+      }
+    else
+      {
+        user: nil,
+        errors: user.errors.full_messages
+      }
+    end
+  end
+end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,7 +3,8 @@ module Types
     field :id, ID, null: false
     field :email, String, null: true
     field :name, String, null: true
-    field :password_digest, String, null: true
+    field :password, String, null: true
+    field :password_confirmation, String, null: true
     field :dogs, [Types::DogType], null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
