42c2e296f50e55a569cba3b1f67d1282de95d2e2
Emmy Morris <emmymorris1102@gmail.com>
2021-08-26 20:00:17 -0600

    Seed database with events and create event type

--- app/graphql/types/event_type.rb
+++ app/graphql/types/event_type.rb
@@ -1,11 +1,10 @@
 module Types
-  class DogType < Types::BaseObject
+  class EventType < Types::BaseObject
     field :id, ID, null: false
-    field :user_id, Integer, null: true
-    field :breed, String, null: true
+    field :dog_id, Integer, null: true
     field :name, String, null: true
-    field :age, Integer, null: true
-    field :events, [Types::EventType], null: true
+    field :completed, Boolean, null: true
+    field :event_datetime, GraphQL::Types::ISO8601DateTime, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,10 +17,18 @@
   )
 
   5.times do
-      user.dogs.create(
+      dog = user.dogs.create(
           name: Faker::Creature::Dog.name,
           age: rand(1..24),
           breed: Faker::Creature::Dog.breed
       )
+
+    5.times do
+      dog.events.create(
+        name: Faker::Verb.base,
+        completed: Faker::Boolean.boolean(true_ratio: 0.2),
+        event_datetime: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+      )
+    end
   end
 end
