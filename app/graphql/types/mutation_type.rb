a902477789353679efc385918195f071550f9b1c
Emmy Morris <emmymorris1102@gmail.com>
2021-08-27 16:08:53 -0600

    Create dog mutation file

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::CreateUser
+    field :create_dog, mutation: Mutations::CreateDog
     # field :create_link, mutation: Mutations::CreateLink
   end
 end
