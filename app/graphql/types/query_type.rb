b38c5105ac815bb343dc0064bfa715dafec7fb35
Emmy Morris <emmymorris1102@gmail.com>
2021-08-27 16:32:32 -0600

    Add create dog functionality

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -16,5 +16,18 @@ module Types
     def user(id:)
       User.find(id)
     end
+
+    field :dogs, [Types::DogType], null: false
+    def dogs
+      Dog.all
+    end
+
+    field :dog, Types::DogType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def dog(id:)
+      Dog.find(id)
+    end
   end
 end
