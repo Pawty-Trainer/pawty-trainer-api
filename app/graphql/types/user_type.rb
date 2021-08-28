204ee265c6252a0cba0f8383ee06ef7437a96bec
ignored-comment <info.konham@gmail.com>
2021-08-28 18:45:32 -0400

    Remove User model validations for email and password w/ reflecting graphQL changes

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,10 +1,7 @@
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
-    field :email, String, null: true
     field :name, String, null: true
-    field :password, String, null: true
-    field :password_confirmation, String, null: true
     field :dogs, [Types::DogType], null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_012942) do
+ActiveRecord::Schema.define(version: 2021_08_28_223210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,9 +36,7 @@ ActiveRecord::Schema.define(version: 2021_08_27_012942) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
     t.string "name"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
