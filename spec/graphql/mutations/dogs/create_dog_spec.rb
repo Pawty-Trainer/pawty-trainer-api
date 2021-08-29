dcb656375eb0fbbcc0ef909f272a5e9f05e6e04a
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-29 16:42:58 -0600

    create test for creating dog

---
+++ spec/graphql/mutations/dogs/create_dog_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+module Mutations
+  module Dogs
+    RSpec.describe CreateDog, type: :request do
+      describe '.resolve' do
+        before :each do
+          User.destroy_all
+          @user = create(:user)
+        end
+
+        # it 'creates a dog' do
+        #   user = create(:user)
+        #   post '/graphql', params: { query: query(user_id: user.id) }
+        # end.to change { Dog.count }.by(1)
+
+        it 'creates a dog' do
+          post '/graphql', params: { query: query }
+          json = JSON.parse(response.body, symbolize_names: true)
+          data = json[:data][:createDog][:dog]
+
+          expect(data).to eq({
+            id: Dog.last.id.to_s,
+            name: 'Roger',
+            breed: 'Poodle',
+            age: 9,
+            userId: @user.id
+          })
+        end
+
+        def query
+          <<~GQL
+            mutation {
+              createDog(
+                input: {
+                  userId: #{@user.id}
+                  name: "Roger"
+                  breed: "Poodle"
+                  age: 9
+                }
+              ) {
+                dog {
+                  id
+                  name
+                  breed
+                  age
+                  userId
+                }
+              }
+            }
+          GQL
+        end
+      end
+    end
+  end
+end
