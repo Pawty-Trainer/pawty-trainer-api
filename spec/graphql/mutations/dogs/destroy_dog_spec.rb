c38a5b3f891dd86497f95978a19f67f3084de1bd
Emmy Morris <emmymorris1102@gmail.com>
2021-08-30 16:46:45 -0600

    Pass dog and event update tests and create dog delete test

--- spec/graphql/mutations/dogs/destroy_dog_spec.rb
+++ spec/graphql/mutations/dogs/destroy_dog_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 module Mutations
   module Dogs
-    RSpec.describe UpdateDog, type: :request do
+    RSpec.describe DestroyDog, type: :request do
       describe '.resolve' do
         before :each do
           User.destroy_all
           @user = create(:user)
           @dog = create(:dog, user: @user, name: 'Hero', age: 4, breed: 'Poodle')
-
         end
 
         # it 'updates a dog data' do
@@ -23,33 +22,25 @@ module Mutations
         #   )
         # end
 
-        it 'update a dog' do
+        xit 'deletes a dog' do
           post '/graphql', params: { query: query(id: @dog.id) }
 
           json = JSON.parse(response.body, symbolize_names: true)
-          # binding.pry
-          data = json[:data][:updateDog][:dog]
+          data = json[:data][:destroyDog][:dog]
 
-          expect(data).to eq({
-            id: "#{@dog.id}",
-            name: "Fido",
-            breed: "Aussie",
-            age: 3,
-            userId: @user.id
-          })
+          expect(data).to eq nil
+          # ({
+          #   id: "#{@dog.id}"
+          # })
+
+          expect(@dog.name).to eq(nil)
         end
 
         def query(id:)
           <<~GQL
           mutation {
-            updateDog(input: { id: #{@dog.id}, name: "Fido", age: "3", breed: "Aussie"}) {
-              dog {
-                  id
-                  userId
-                  name
-                  breed
-                  age
-              }
+            destroyDog(input: {id: #{@dog.id}}) {
+              id
             }
           }
           GQL
